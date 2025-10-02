58e5612c1f1ac2313c197508e55e845b52668462
Passing01 <147635730+Passing01@users.noreply.github.com>
2025-10-02 15:54:43 +0000

    first commit

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 composer install --no-dev --optimize-autoloader
+npm install
+npm run build
+
+mkdir -p storage/framework/{sessions,views,cache}
+mkdir -p storage/logs
 
 # Créer le répertoire pour la base de données
 mkdir -p /opt/render/project/.data
@@ -10,6 +15,13 @@ touch /opt/render/project/.data/database.sqlite
 
 # Donner les permissions
 chmod 664 /opt/render/project/.data/database.sqlite
+
+chmod -R 775 storage bootstrap/cache
+
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
 
 # Exécuter les migrations
 php artisan migrate --force
